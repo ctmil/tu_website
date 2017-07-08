5101b37319a39c0b24770eb158065db7913108a9
morrillo <gustavo.orrillo@gmail.com>
2017-07-07 21:00:21 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'website tupulperia',
+    'category': 'Hidden',
+    'summary': 'website tupulperia',
+    'version': '1.0',
+    'description': """website tupulperia""",
+    'author': 'Moldeo Interactive - www.moldeo.coop',
+    'depends': ['website_sale'],
+    'data': [
+        'views/tu_website_views.xml'
+    ],
+    'qweb': [
+        'static/src/xml/templates.xml'
+    ],
+    'installable': True,
+}
