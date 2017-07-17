872d27fc6f5bf42a1374011da2083617899fe2f4
morrillo <gustavo.orrillo@gmail.com>
2017-07-17 20:09:11 -0300

    Fixed bug

--- __openerp__.py
+++ __openerp__.py
@@ -6,13 +6,13 @@
     'summary': 'website tupulperia',
     'version': '1.0',
     'description': """website tupulperia""",
-    'author': 'Moldeo Interactive - www.moldeo.coop',
+    'author': 'Moldeo Interactive - www.moldeointeractive.com.ar',
     'depends': ['website','website_sale','product'],
     'data': [
-        'views/tu_website_view.xml'
+        'views/tu_website_view.xml',
+	'views/templates.xml'
     ],
     'qweb': [
-	'static/src/xml/templates.xml'
     ],
     'installable': True,
 }
